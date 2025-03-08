dd031a272f51148b821e82beda2aead71313e4f9
bbaez021 <bbaez021@gmail.com>
2025-03-07 21:24:20 -0600

    Added song schema + changed types def

--- server/src/models/song.ts
+++ server/src/models/song.ts
@@ -1,2 +1,30 @@
 import mongoose from 'mongoose';
 
+var SongSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        default: "Unknown Track"
+    },
+    artist: {
+        type: String,
+        default: "D.A.R."
+    },
+    releaseYear: {
+        type: Number,
+        required: false
+    },
+    albumInfo: {
+        type: String,
+        default: "../public/default.jpg"  
+    },
+    lastAppeared: {
+        type: Date,
+        required: false
+    },
+    timesAppeared: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('Song', SongSchema);
